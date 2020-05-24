bf0be4d4a4acfd9e135b010859df469d14158dde
Shaun Jackson <shaunbjackson@gmail.com>
2020-05-24 10:59:04 -0700

    [BlOG-39] feat(mobile-nav): media query and wide nav component

--- src/components/Nav/NavMenu.js
+++ src/components/Nav/NavMenu.js
@@ -2,6 +2,7 @@ import React from "react";
 import ToogleMode from "./ToogleMode";
 import { Link } from "gatsby";
 import siteConfig from "../../../data/SiteConfig";
+import WideNavMenu from "./WideNavMenu";
 
 /** @jsx jsx */
 import { Styled, jsx } from "theme-ui";
@@ -55,50 +56,13 @@ class NavMenu extends React.Component {
         >
           <div
             sx={{
-              display: `flex`,
-              justifyContent: `space-between`,
-              alignItems: `center`,
-              maxWidth: `container`,
               height: "100%",
-              m: "auto",
+              "@media screen and (max-width: 600px)": {
+                display: "none",
+              },
             }}
           >
-            <Link
-              to="/"
-              sx={{
-                textDecoration: `none`,
-                color: `panelText`,
-                transition: "all .3s ease",
-                "&:hover": {
-                  color: lighten("primary", 0.3),
-                },
-              }}
-            >
-              <div sx={{ pl: "1rem" }}>{siteTitle}</div>
-            </Link>
-            <div style={{ display: `flex`, alignItems: `center` }}>
-              {menuLinks.map((link) => {
-                return (
-                  <Styled.h3
-                    key={link.url}
-                    as={Link}
-                    to={link.url}
-                    sx={{
-                      mr: 3,
-                      textDecoration: `none`,
-                      color: "panelText",
-                      transition: "all .3s ease",
-                      "&:hover": {
-                        color: lighten("primary", 0.3),
-                      },
-                    }}
-                  >
-                    {link.name}
-                  </Styled.h3>
-                );
-              })}
-              <ToogleMode />
-            </div>
+            <WideNavMenu menuLinks={menuLinks} siteTitle={siteTitle} />
           </div>
         </Styled>
       </nav>
---
+++ src/components/Nav/WideNavMenu.js
@@ -0,0 +1,66 @@
+import React from "react";
+import ToogleMode from "./ToogleMode";
+import { Link } from "gatsby";
+import siteConfig from "../../../data/SiteConfig";
+
+/** @jsx jsx */
+import { Styled, jsx } from "theme-ui";
+import { darken, lighten } from "@theme-ui/color";
+
+class WideNavMenu extends React.Component {
+  render() {
+    const { menuLinks, siteTitle } = this.props;
+
+    return (
+      <div
+        sx={{
+          display: `flex`,
+          justifyContent: `space-between`,
+          alignItems: `center`,
+          maxWidth: `container`,
+          height: "100%",
+          m: "auto",
+        }}
+      >
+        <Link
+          to="/"
+          sx={{
+            textDecoration: `none`,
+            color: `panelText`,
+            transition: "all .3s ease",
+            "&:hover": {
+              color: lighten("primary", 0.3),
+            },
+          }}
+        >
+          <div sx={{ pl: "1rem" }}>{siteTitle}</div>
+        </Link>
+        <div style={{ display: `flex`, alignItems: `center` }}>
+          {menuLinks.map((link) => {
+            return (
+              <Styled.h3
+                key={link.url}
+                as={Link}
+                to={link.url}
+                sx={{
+                  mr: 3,
+                  textDecoration: `none`,
+                  color: "panelText",
+                  transition: "all .3s ease",
+                  "&:hover": {
+                    color: lighten("primary", 0.3),
+                  },
+                }}
+              >
+                {link.name}
+              </Styled.h3>
+            );
+          })}
+          <ToogleMode />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default WideNavMenu;
