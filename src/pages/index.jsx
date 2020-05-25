e414eb790ddec13e0fb9e32a6257a543299900ed
Shaun Jackson <shaunbjackson@gmail.com>
2020-05-24 23:12:11 -0700

    [BLOG-37] chore(refactor): index page to hook

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,119 +9,108 @@ import config from "../../data/SiteConfig";
 
 /** @jsx jsx */
 import { Styled, jsx } from "theme-ui";
-import { darken, lighten } from "@theme-ui/color";
+import { lighten } from "@theme-ui/color";
 
-class Index extends React.Component {
-  render() {
-    const horizontalMostRecentEdges = this.props.data.horizontalMostRecent
-      .edges;
-    const horizontalMostPopular = this.props.data.horizontalMostPopular.edges;
-    const verticalMostRecentEdges = this.props.data.verticalMostRecent.edges;
-    const verticalMostPopularEdges = this.props.data.verticalMostPopular.edges;
-    const { userLinks } = config;
-
-    return (
-      <Layout>
-        <Helmet title={config.siteTitle} />
-        <SEO />
-        <section>
-          <div
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              p: "2em",
-            }}
-          >
-            <Styled.h1 sx={{ m: ".5em" }}>Hi! I'm Shaun</Styled.h1>
-            <Styled.p sx={{ textAlign: "center" }}>
-              I'm a Software Engineer passionate about modern Javascript,
-              Backend technologies, Data, and Cloud tooling.
-            </Styled.p>
-            <a
-              href="https://shaunjacks.substack.com"
-              sx={{
-                padding: "1em",
-                borderRadius: ".5rem",
-                textDecoration: "none",
-                color: "background",
-                cursor: "pointer",
-                outline: "none",
-                backgroundColor: "primary",
-                border: "3px solid text",
-                borderColor: "primary",
-                borderRadius: "5px",
-                borderWidth: "1px",
-                padding: "4px 11px",
-                fontSize: "16px",
-                boxShadow: "0 2px 0 rgba(0,0,0,.045)",
-                transition: "all .3s ease",
-                "&:focus, &:hover": {
-                  outline: "none",
-                  borderColor: lighten("primary", 0.2),
-                  backgroundColor: lighten("primary", 0.2),
-                },
-              }}
-            >
-              <Styled.h3 sx={{ m: 0, p: ".5em 1.5em", color: "background" }}>
-                Subscribe
-              </Styled.h3>
-            </a>
-          </div>
-        </section>
-        <section
+export default function Index({ data: { mostRecent, mostPopular } }) {
+  return (
+    <Layout>
+      <Helmet title={config.siteTitle} />
+      <SEO />
+      <section>
+        <div
           sx={{
-            my: `40px`,
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
+            p: "2em",
           }}
         >
-          <Styled.h1 sx={{ mt: 0, color: "text" }}>Most popular</Styled.h1>
-          <PostListing postEdges={verticalMostPopularEdges} />
-        </section>
-        <section
-          sx={{
-            my: `40px`,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Styled.h1 sx={{ mt: 0, color: "text" }}>Most recent</Styled.h1>
-          <PostListing postEdges={verticalMostRecentEdges} />
-        </section>
-        <section sx={{ display: "flex", justifyContent: "center" }}>
-          <Styled
-            as={Link}
-            to={"/blog"}
+          <Styled.h1 sx={{ m: ".5em" }}>Hi! I'm Shaun</Styled.h1>
+          <Styled.p sx={{ textAlign: "center" }}>
+            I'm a Software Engineer passionate about modern Javascript, Backend
+            technologies, Data, and Cloud tooling.
+          </Styled.p>
+          <a
+            href="https://shaunjacks.substack.com"
             sx={{
-              margin: `auto`,
-              textAlign: `center`,
-              color: `primary`,
-              textDecoration: `none`,
+              padding: "1em",
+              borderRadius: ".5rem",
+              textDecoration: "none",
+              color: "background",
+              cursor: "pointer",
+              outline: "none",
+              backgroundColor: "primary",
+              border: "3px solid text",
+              borderColor: "primary",
+              borderRadius: "5px",
+              borderWidth: "1px",
+              padding: "4px 11px",
+              fontSize: "16px",
+              boxShadow: "0 2px 0 rgba(0,0,0,.045)",
               transition: "all .3s ease",
-              fontSize: 20,
-              p: ".5em 1.5em",
-              ":hover": {
-                color: lighten("primary", 0.2),
+              "&:focus, &:hover": {
+                outline: "none",
+                borderColor: lighten("primary", 0.2),
+                backgroundColor: lighten("primary", 0.2),
               },
             }}
           >
-            More posts...
-          </Styled>
-        </section>
-      </Layout>
-    );
-  }
+            <Styled.h3 sx={{ m: 0, p: ".5em 1.5em", color: "background" }}>
+              Subscribe
+            </Styled.h3>
+          </a>
+        </div>
+      </section>
+      <section
+        sx={{
+          my: `40px`,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Styled.h1 sx={{ mt: 0, color: "text" }}>Most popular</Styled.h1>
+        <PostListing postEdges={mostPopular.edges} />
+      </section>
+      <section
+        sx={{
+          my: `40px`,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Styled.h1 sx={{ mt: 0, color: "text" }}>Most recent</Styled.h1>
+        <PostListing postEdges={mostRecent.edges} />
+      </section>
+      <section sx={{ display: "flex", justifyContent: "center" }}>
+        <Styled
+          as={Link}
+          to={"/blog"}
+          sx={{
+            margin: `auto`,
+            textAlign: `center`,
+            color: `primary`,
+            textDecoration: `none`,
+            transition: "all .3s ease",
+            fontSize: 20,
+            p: ".5em 1.5em",
+            ":hover": {
+              color: lighten("primary", 0.2),
+            },
+          }}
+        >
+          More posts...
+        </Styled>
+      </section>
+    </Layout>
+  );
 }
-
-export default Index;
 
 /* eslint no-undef: "off" */
 export const pageQuery = graphql`
   query IndexQuery {
-    verticalMostRecent: allMdx(
+    mostRecent: allMdx(
       limit: 4
       sort: { fields: [fields___date], order: DESC }
       filter: {
@@ -152,7 +141,7 @@ export const pageQuery = graphql`
         }
       }
     }
-    verticalMostPopular: allMdx(
+    mostPopular: allMdx(
       limit: 4
       sort: { fields: [fields___date], order: DESC }
       filter: {
@@ -160,72 +149,6 @@ export const pageQuery = graphql`
           publish: { eq: "yes" }
           template: { eq: "post" }
           popular: { eq: true }
-        }
-      }
-    ) {
-      edges {
-        node {
-          fields {
-            slug
-            date(formatString: "MMMM Do, YYYY")
-          }
-          excerpt
-          timeToRead
-          frontmatter {
-            category
-            title
-            tags
-            cover {
-              childImageSharp {
-                fixed(width: 100, height: 100) {
-                  ...GatsbyImageSharpFixed
-                }
-              }
-            }
-            date
-          }
-        }
-      }
-    }
-    horizontalMostRecent: allMdx(
-      limit: 4
-      sort: { fields: [fields___date], order: DESC }
-      filter: {
-        frontmatter: { publish: { eq: "yes" }, template: { eq: "post" } }
-      }
-    ) {
-      edges {
-        node {
-          fields {
-            slug
-            date(formatString: "MMMM Do, YYYY")
-          }
-          excerpt
-          timeToRead
-          frontmatter {
-            category
-            title
-            tags
-            cover {
-              childImageSharp {
-                fixed(width: 100, height: 100) {
-                  ...GatsbyImageSharpFixed
-                }
-              }
-            }
-            date
-          }
-        }
-      }
-    }
-    horizontalMostPopular: allMdx(
-      limit: 4
-      sort: { fields: [fields___date], order: DESC }
-      filter: {
-        frontmatter: {
-          popular: { eq: true }
-          publish: { eq: "yes" }
-          template: { eq: "post" }
         }
       }
     ) {
