f44e531803c2dc7085af7fe0717ea7ae639aab68
Shaun Jackson <shaunbjackson@gmail.com>
2020-05-29 23:52:51 -0700

    [BLOG-43] feat(typescript): updated Disqus to typescript

--- src/components/Disqus/Disqus.tsx
+++ src/components/Disqus/Disqus.tsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { DiscussionEmbed } from "disqus-react";
+import { PostNode } from "../Post/Post.model";
 
-export default function Disqus({ postNode }) {
+export default function Disqus({ postNode }: { postNode: PostNode }) {
   const disqusConfig = {
-    shortname: process.env.disqusShortName,
+    shortname: process.env.disqusShortName as string,
     config: {
+      url: "",
       identifier: postNode.fields.slug,
       title: postNode.frontmatter.title,
     },
