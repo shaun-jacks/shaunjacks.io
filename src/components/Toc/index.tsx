fe3b68ca8ee11009a171edf8d110df311cd41320
Shaun Jackson <shaunbjackson@gmail.com>
2020-06-26 18:52:18 -0700

    [BLOG-40] feat(TOC): added toc component

---
+++ src/components/Toc/index.tsx
@@ -0,0 +1,113 @@
+import React, { useRef, useState, useEffect } from "react";
+import { throttle } from "lodash";
+import { Link } from "gatsby";
+
+/** @jsx jsx */
+import { Styled, jsx } from "theme-ui";
+import { lighten } from "@theme-ui/color";
+
+const accumulateOffsetTop = (el: any, totalOffset = 0) => {
+  while (el) {
+    totalOffset += el.offsetTop - el.scrollTop + el.clientTop;
+    el = el.offsetParent;
+  }
+  return totalOffset;
+};
+
+export interface TocProps {
+  headingSelector?: string | string[];
+  getTitle?: (val: Node) => string;
+  getDepth?: (val: Node) => number;
+  throttleTime?: number;
+  tocTitle?: string;
+}
+
+export default function Toc({
+  headingSelector,
+  getTitle,
+  getDepth,
+  ...rest
+}: TocProps) {
+  const { throttleTime = 200, tocTitle = `Contents` } = rest;
+  const [headings, setHeadings] = useState<{
+    titles: any[];
+    nodes: any[];
+    minDepth: number;
+  }>({
+    titles: [],
+    nodes: [],
+    minDepth: 0,
+  });
+  // Controls which heading is currently highlighted as active.
+  const [active, setActive] = useState<number | void>();
+  useEffect(() => {
+    const nodes = Array.from(document.querySelectorAll("main h1, main h2"));
+    const titles = nodes.map((node: any) => ({
+      title: node.innerText,
+      depth: Number(node.nodeName[1]),
+    }));
+    // Compute the minimum heading depth. Will be subtracted from each heading's
+    // depth to determine the indentation of that heading in the ToC.
+    const minDepth = Math.min(...titles.map((h) => h.depth));
+    setHeadings({ titles, nodes, minDepth });
+  }, [headingSelector, getTitle, getDepth]);
+
+  // Add scroll event listener to update currently active heading.
+  useEffect(() => {
+    // Throttling the scrollHandler saves computation and hence battery life.
+    const scrollHandler = throttle(() => {
+      const { titles, nodes } = headings;
+      // Offsets need to be recomputed inside scrollHandler because
+      // lazily-loaded content increases offsets as user scrolls down.
+      const offsets = nodes.map((el) => accumulateOffsetTop(el));
+      const activeIndex = offsets.findIndex(
+        (offset) => offset > window.scrollY + 0.8 * window.innerHeight
+      );
+      setActive(activeIndex === -1 ? titles.length - 1 : activeIndex - 1);
+    }, throttleTime);
+
+    window.addEventListener(`scroll`, scrollHandler);
+    return () => window.removeEventListener(`scroll`, scrollHandler);
+  }, [headings]);
+
+  return (
+    <Styled.div
+      sx={{
+        px: 2,
+        py: 2,
+      }}
+    >
+      <Styled.h3>{`Contents`}</Styled.h3>
+      <nav>
+        {headings.titles.map(({ title, depth }, index) => {
+          console.log(title, depth);
+          return (
+            <Styled.p
+              key={title}
+              sx={{
+                cursor: "pointer",
+                color: "text",
+                "&.active": {
+                  color: "primary",
+                  fontWeight: "bold",
+                },
+                display: "block",
+                marginLeft: `${depth - 1 + `em`}`,
+              }}
+              className={active === index ? "active" : ""}
+              onClick={(event: any) => {
+                event.preventDefault();
+                headings.nodes[index].scrollIntoView({
+                  behavior: `smooth`,
+                  block: `center`,
+                });
+              }}
+            >
+              {title}
+            </Styled.p>
+          );
+        })}
+      </nav>
+    </Styled.div>
+  );
+}
